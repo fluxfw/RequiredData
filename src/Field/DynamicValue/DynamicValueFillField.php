5b18f3871451afae7ff45764ae20166f5d3202e9
Florian Wyss <fw@studer-raimann.ch>
2020-01-22 14:04:29 +0100

    `DynamicValue`

---
+++ src/Field/DynamicValue/DynamicValueFillField.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace srag\RequiredData\Field\DynamicValue;
+
+use ilNonEditableValueGUI;
+use srag\CustomInputGUIs\PropertyFormGUI\PropertyFormGUI;
+use srag\RequiredData\Fill\AbstractFillField;
+
+/**
+ * Class DynamicValueFillField
+ *
+ * @package srag\RequiredData\Field\DynamicValue
+ *
+ * @author  studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
+ */
+abstract class DynamicValueFillField extends AbstractFillField
+{
+
+    /**
+     * @var DynamicValueField
+     */
+    protected $field;
+
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(DynamicValueField $field)
+    {
+        parent::__construct($field);
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getFormFields() : array
+    {
+        return [
+            PropertyFormGUI::PROPERTY_CLASS => ilNonEditableValueGUI::class,
+            PropertyFormGUI::PROPERTY_VALUE => $this->field->deliverDynamicValue()
+        ];
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function formatAsJson($fill_value)
+    {
+        return $this->field->deliverDynamicValue();
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function formatAsString($fill_value) : string
+    {
+        return strval($fill_value);
+    }
+}
