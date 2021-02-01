14060e01213fd8dc41dbd1af3ee6e8d4427b2320
sragfw <fw@studer-raimann.ch>
2021-02-01 09:01:47 +0100

    Fix ctrl definitions in wrong file

--- src/Field/Field/Group/class.GroupCtrl.php
+++ src/Field/Field/Group/class.GroupCtrl.php
@@ -14,7 +14,6 @@ use srag\RequiredData\Field\FieldCtrl;
  * @author            studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
  *
  * @ilCtrl_isCalledBy srag\RequiredData\Field\Field\Group\GroupCtrl: srag\RequiredData\Field\Field\Group\GroupsCtrl
- * @ilCtrl_isCalledBy srag\RequiredData\Field\Field\StaticMultiSearchSelect\SMSSAjaxAutoCompleteCtrl: srag\RequiredData\Field\Field\Group\GroupCtrl
  */
 class GroupCtrl extends FieldCtrl
 {
